696e623905ed410f1cca8e4840c8802b523f73f2
Diego Fernando Rodriguez <diegodfrf@gmail.com>
2024-05-07 16:14:19 -0500

    Remove comments

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,11 +1,3 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
@@ -13,8 +5,6 @@
 AudioPluginFadeInVolumeEffectAudioProcessorEditor::AudioPluginFadeInVolumeEffectAudioProcessorEditor (AudioPluginFadeInVolumeEffectAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
 }
 
@@ -25,7 +15,6 @@ AudioPluginFadeInVolumeEffectAudioProcessorEditor::~AudioPluginFadeInVolumeEffec
 //==============================================================================
 void AudioPluginFadeInVolumeEffectAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setColour (juce::Colours::white);
@@ -35,6 +24,4 @@ void AudioPluginFadeInVolumeEffectAudioProcessorEditor::paint (juce::Graphics& g
 
 void AudioPluginFadeInVolumeEffectAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
 }
